162748bb66399828acc9a5ae4822fedecde895d3
rodrigoanasco <ro.anasco.s@gmail.com>
2026-06-28 01:14:16 -0700

    added description and table

---
+++ eisenhower-matrix/components/matrix.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const matrix = () => {
+  return (
+    <section className="matrix">
+      <div className="quadrant left-box top-box inside-box-titles">Do</div>
+      <div className="quadrant top-box inside-box-titles">Schedule</div>
+      <div className="quadrant left-box inside-box-titles">Delegate</div>
+      <div className="quadrant inside-box-titles">Delete</div>
+    </section>
+  );
+};
+
+export default matrix;
--- eisenhower-matrix/src/app/page.tsx
+++ eisenhower-matrix/src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import React from "react";
+import Matrix from "../../components/matrix";
 
 export default function Home() {
   return (
-    <main className="main-title">
-      <h1>Eisenhower Matrix</h1>
+    <main>
+      <h1 className="main-title">Eisenhower Matrix</h1>
+      <p className="page-description">
+        The best way to set up your priorities is through this matrix, click the
+        plus sign to create your block, then drag it where you think it belongs.
+        <br />
+        You can also add a description, deadline or even some steps to follow!
+      </p>
+      <Matrix />
     </main>
   );
 }
-
-
