6d0a6cebf2165a6941de7578d974d6728a9cbac0
rodrigoanasco <ro.anasco.s@gmail.com>
2026-07-02 23:57:39 -0700

    making fillable to pop up when each task

---
+++ eisenhower-matrix/elements/FillTask.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState } from "react";
+
+const FillTask = () => {
+  const [taskName, setTaskName] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <>
+      <div>FillTask</div>
+    </>
+  );
+};
+
+export default FillTask;
