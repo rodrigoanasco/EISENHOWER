c6b15b4e3654d010e051ad7f5b9979842c50f63c
rodrigoanasco <ro.anasco.s@gmail.com>
2026-07-03 17:55:06 -0700

    finished task fillable

--- eisenhower-matrix/elements/FillTask.jsx
+++ eisenhower-matrix/elements/FillTask.jsx
@@ -2,14 +2,41 @@
 
 import React, { useState } from "react";
 
-const FillTask = () => {
+const FillTask = ({ closePopup }) => {
   const [taskName, setTaskName] = useState("");
   const [description, setDescription] = useState("");
+  const [deadline, setDeadline] = useState("");
 
   return (
-    <>
-      <div>FillTask</div>
-    </>
+    <div className="popup-background">
+      <form className="popup-box">
+        <h2>Edit Task</h2>
+
+        <input
+          type="text"
+          placeholder="Task name"
+          value={taskName}
+          onChange={(event) => setTaskName(event.target.value)}
+        />
+
+        <textarea
+          placeholder="Write task description..."
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+
+        <input
+          type="date"
+          value={deadline}
+          onChange={(event) => setDeadline(event.target.value)}
+        />
+
+        <button type="button">Save</button>
+        <button type="button" onClick={closePopup}>
+          Cancel
+        </button>
+      </form>
+    </div>
   );
 };
 
